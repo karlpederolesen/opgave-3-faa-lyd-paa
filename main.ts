c9d7fbe8a432abec77e91667d5d278a83231db85
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2021-12-25 18:06:58 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-input.onButtonPressed(Button.A, function () {
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C D E C C D E C ", 120)
 })
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    for (let index = 0; index < 2; index++) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Double))
+    }
+})
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
+basic.clearScreen()
